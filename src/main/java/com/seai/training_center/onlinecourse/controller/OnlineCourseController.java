9a3925c785102ab7bb725590b4aa210ab88e4af9
Vasil Kostadinov <vasil.dm.kostadinov@gmail.com>
2024-02-04 18:31:28 +0200

    Start implementing training centers

---
+++ src/main/java/com/seai/training_center/onlinecourse/controller/OnlineCourseController.java
@@ -0,0 +1,11 @@
+package com.seai.training_center.onlinecourse.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1")
+public class OnlineCourseController {
+}
--- src/main/java/com/seai/training_center/user/controller/TrainingCenterController.java
+++ src/main/java/com/seai/training_center/user/controller/TrainingCenterController.java
@@ -1,15 +1,8 @@
 package com.seai.training_center.user.controller;
 
-import com.seai.marine.user.contract.request.UserUpdateRequest;
-import com.seai.marine.user.contract.response.GetUserResponse;
-import com.seai.marine.user.mapper.UserAuthenticationMapper;
-import com.seai.marine.user.mapper.UserMapper;
-import com.seai.marine.user.model.User;
 import com.seai.marine.user.model.UserAuthentication;
 import com.seai.marine.user.repository.UserAuthenticationRepository;
-import com.seai.marine.user.repository.UserRepository;
 import com.seai.training_center.user.contract.request.CreateTrainingCenterRequest;
-import com.seai.training_center.user.contract.request.UpdateTrainingCenterRequest;
 import com.seai.training_center.user.contract.response.GetTrainingCenterResponse;
 import com.seai.training_center.user.mapper.TrainingCenterAuthenticationMapper;
 import com.seai.training_center.user.mapper.TrainingCenterMapper;
@@ -20,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,16 +29,9 @@ public class TrainingCenterController {
     private final UserAuthenticationRepository userAuthenticationRepository;
     private final TrainingCenterAuthenticationMapper trainingCenterAuthenticationMapper;
 
-//    @PutMapping("/training-centers/{trainingCenterId}")
-//    public void updateTrainingCenter(@RequestBody @Valid UpdateTrainingCenterRequest updateTrainingCenterRequest, @PathVariable UUID trainingCenterId) {
-//        trainingCenterRepository.findById(userId);
-//        User user = userMapper.map(userRegisterRequest);
-//        userRepository.update(userId, user);
-//    }
-
     @GetMapping("/training-centers/{trainingCenterId}")
-    public GetTrainingCenterResponse getTrainingCenter(@PathVariable UUID userId) {
-        TrainingCenter trainingCenter = trainingCenterRepository.findById(userId);
+    public GetTrainingCenterResponse getTrainingCenter(@PathVariable UUID trainingCenterId) {
+        TrainingCenter trainingCenter = trainingCenterRepository.findById(trainingCenterId);
         return trainingCenterMapper.map(trainingCenter);
     }
 
