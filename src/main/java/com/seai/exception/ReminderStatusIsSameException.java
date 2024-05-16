87f662513e468c80f0fb54d994ec632fc886f8b4
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-05-16 12:03:04 +0300

    New custom reminder exception

---
+++ src/main/java/com/seai/exception/ReminderStatusIsSameException.java
@@ -0,0 +1,8 @@
+package com.seai.exception;
+
+public class ReminderStatusIsSameException extends GeneralException {
+
+    public ReminderStatusIsSameException(String message, Object... args) {
+        super(message, args);
+    }
+}
