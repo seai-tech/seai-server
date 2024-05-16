182f50bf8636eed83707ee00dc0102eaef710ade
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-05-16 11:58:00 +0300

    The class which creates the mail message

---
+++ src/main/java/com/seai/marine/notification/email/EmailSender.java
@@ -0,0 +1,34 @@
+package com.seai.marine.notification.email;
+
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSender {
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    private final JavaMailSender mailSender;
+
+    public EmailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendSimpleMessage(String to, String subject, String text) {
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper mimeHelper = new MimeMessageHelper(mimeMessage, true);
+            mimeHelper.setFrom(fromEmail);
+            mimeHelper.setTo(to);
+            mimeMessage.setSubject(subject);
+            mimeMessage.setText(text);
+            mailSender.send(mimeMessage);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
