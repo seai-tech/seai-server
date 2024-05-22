d99e0dfa9aa9a47a5d5e1bfab95c0095ef53dbef
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-05-22 11:32:52 +0300

    fixed the method from username to id

--- src/main/java/com/seai/marine/user/repository/UserAuthenticationRepository.java
+++ src/main/java/com/seai/marine/user/repository/UserAuthenticationRepository.java
@@ -25,9 +25,11 @@ public class UserAuthenticationRepository {
 
     private static final String FIND_USER_BY_EMAIL_QUERY = "SELECT id, email, password FROM users_auth WHERE email= ?";
 
-    private static final String FIND_USER_BY_USERNAME_QUERY = "SELECT id, email, username, password FROM users_auth WHERE username= ?";
+    private static final String FIND_USER_BY_ID_QUERY = "SELECT id, email, password FROM users_auth WHERE id= ?";
 
     private static final String DELETE_USER = "DELETE FROM users_auth WHERE id= ?";
+
+
 
     private final PasswordEncoder encoder;
 
@@ -61,18 +63,17 @@ public class UserAuthenticationRepository {
         }
     }
 
-    public UserAuthentication findByUsername(String username) throws UsernameNotFoundException {
+    public UserAuthentication findById(UUID userId) throws UsernameNotFoundException {
         try {
-            return jdbcTemplate.queryForObject(FIND_USER_BY_USERNAME_QUERY,
+            return jdbcTemplate.queryForObject(FIND_USER_BY_ID_QUERY,
                     (rs, rowNum) -> new UserAuthentication(
                             UUID.fromString(rs.getString("id")),
                             rs.getString("email"),
-                            rs.getString("password")), username);
+                            rs.getString("password")), userId.toString());
         } catch (EmptyResultDataAccessException ex) {
-            throw new ResourceNotFoundException("Invalid username : " + username);
+            throw new ResourceNotFoundException("Invalid user with id : " + userId);
         }
     }
-
     public void delete(UUID uuid) {
         jdbcTemplate.update(DELETE_USER, uuid.toString());
     }
