36113f733985d23be1a1731d87f4fdc16fb6f049
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-05-22 11:17:50 +0300

    Created response for creating ships

---
+++ src/main/java/com/seai/ship/contract/response/CreateShipResponse.java
@@ -0,0 +1,50 @@
+package com.seai.ship.contract.response;
+
+import lombok.Data;
+
+@Data
+public class CreateShipResponse {
+    private String id;
+
+    private Long imoNumber;
+
+    private String vesselName;
+
+    private String shipType;
+
+    private String flag;
+
+    private String homeport;
+
+    private Integer grossTonnage;
+
+    private Integer summerDeadweight;
+
+    private Double lengthOverall;
+
+    private Double beam;
+
+    private Double draught;
+
+    private Integer yearOfBuild;
+
+    private String builder;
+
+    private String placeOfBuild;
+
+    private String yard;
+
+    private Integer teu;
+
+    private Integer crudeOil;
+
+    private Double gasCapacity;
+
+    private String grain;
+
+    private String bale;
+
+    private String classificationSociety;
+
+    private String registeredOwner;
+}
