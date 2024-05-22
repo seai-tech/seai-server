cb67844260d9274a26eac022db215ae498186164
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-05-22 11:22:31 +0300

    Optimized the scheduler to get all needed info within one query and adjusted if else checks to fit the new criteria

--- src/main/java/com/seai/marine/document/service/DocumentScheduler.java
+++ src/main/java/com/seai/marine/document/service/DocumentScheduler.java
@@ -1,12 +1,8 @@
 package com.seai.marine.document.service;
 
-import com.seai.marine.document.model.MarineDocument;
+import com.seai.marine.document.contract.response.GetDocumentsForReminderResponse;
 import com.seai.marine.document.repository.DocumentRepository;
 import com.seai.marine.notification.email.EmailSender;
-import com.seai.marine.user.model.User;
-import com.seai.marine.user.model.UserAuthentication;
-import com.seai.marine.user.repository.UserAuthenticationRepository;
-import com.seai.marine.user.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -20,29 +16,22 @@ import java.util.List;
 public class DocumentScheduler {
     private final DocumentRepository documentRepository;
     private final EmailSender emailService;
-    private final UserAuthenticationRepository userAuthenticationRepository;
-        private final UserRepository userRepository;
 
-    @Scheduled(cron = "40 47 11 * * ?")
+    @Scheduled(cron = "40 15 11 * * ?")
     public void checkCertificatesDaily() {
         LocalDate today = LocalDate.now();
-        List<UserAuthentication> userAuthenticationUsers = userAuthenticationRepository.findAllUsers();
-        for (UserAuthentication user : userAuthenticationUsers) {
-            User user_details = userRepository.findById(user.getId());
-            if (!user_details.getReminder_subscription()) {
-                continue;
-            }
-            List<MarineDocument> documents = documentRepository.findDocumentsByEmail(user.getEmail());
-
-            for (MarineDocument document : documents) {
-                LocalDate expiryDate = document.getExpiryDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-                LocalDate oneMonthBeforeExp = expiryDate.minusMonths(1);
-
-                if (today.isEqual(oneMonthBeforeExp) || (today.isAfter(oneMonthBeforeExp) && today.isBefore(expiryDate))) {
-                    emailService.sendSimpleMessage(user.getEmail(), "Reminder: Certificate expires in one month or less", "Your certificate is nearing expiration.");
-                } else if (today.isAfter(expiryDate) || today.equals(expiryDate)) {
-                    emailService.sendSimpleMessage(user.getEmail(), "Notice: Certificate has expired", "Your certificate has expired.");
-                }
+        List<GetDocumentsForReminderResponse> documents = documentRepository.findDocumentsForUsersWithReminders();
+        for (GetDocumentsForReminderResponse document : documents) {
+            LocalDate expiryDate = document.getExpiryDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            LocalDate sixtyDaysBeforeExp = expiryDate.minusDays(60);
+            if (today.equals(sixtyDaysBeforeExp)) {
+                emailService.sendSimpleMessage(document.getUserEmail(),
+                        "Reminder: Certificate expires in 60 days",
+                        "Your certificate is nearing expiration in 60 days:" + " " + document.getName() + "Expiring on:" + " "  + document.getExpiryDate() );
+            } else if (today.equals(expiryDate)) {
+                emailService.sendSimpleMessage(document.getUserEmail(),
+                        "Notice: Certificate has expired",
+                        "Your certificate has expired:" + " " +document.getName());
             }
         }
     }
