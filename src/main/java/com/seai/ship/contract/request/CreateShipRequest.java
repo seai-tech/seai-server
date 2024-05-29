97ebdeb8d8438358419d448a9ba0a2efce7aad6c
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-05-29 09:36:46 +0300

    Added validators on the request model

--- src/main/java/com/seai/ship/contract/request/CreateShipRequest.java
+++ src/main/java/com/seai/ship/contract/request/CreateShipRequest.java
@@ -1,33 +1,48 @@
 package com.seai.ship.contract.request;
 
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
+import org.hibernate.validator.constraints.URL;
 
 @Data
 public class CreateShipRequest {
 
+    @NotNull
+    @Digits(integer = 7, fraction = 0)
     private Long imoNumber;
 
+    @NotBlank
     private String vesselName;
 
-
+    @NotBlank
     private String shipType;
 
-
+    @NotBlank
     private String flag;
 
     private String homeport;
 
+    @NotNull
+    @Min(0)
     private Integer grossTonnage;
 
+    @NotNull
+    @Min(0)
     private Integer summerDeadweight;
 
+    @NotNull
+    @Min(0)
     private Double lengthOverall;
 
+    @NotNull
+    @Min(0)
     private Double beam;
 
+    @Min(0)
     private Double draught;
 
+    @NotNull
     private Integer yearOfBuild;
 
     private String builder;
@@ -36,10 +51,13 @@ public class CreateShipRequest {
 
     private String yard;
 
+    @Min(0)
     private Integer teu;
 
+    @Min(0)
     private Integer crudeOil;
 
+    @Min(0)
     private Double gasCapacity;
 
     private String grain;
@@ -52,15 +70,19 @@ public class CreateShipRequest {
 
     private String ownerAddress;
 
+    @Email
     private String ownerEmail;
 
+    @URL
     private String ownerWebsite;
 
     private String manager;
 
     private String managerAddress;
 
+    @Email
     private String managerEmail;
 
+    @URL
     private String managerWebsite;
 }
--- src/main/java/com/seai/ship/contract/request/UpdateShipRequest.java
+++ src/main/java/com/seai/ship/contract/request/UpdateShipRequest.java
@@ -1,9 +1,14 @@
 package com.seai.ship.contract.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.URL;
 
 @Data
 @AllArgsConstructor
@@ -11,32 +16,53 @@ import lombok.NoArgsConstructor;
 @Getter
 public class UpdateShipRequest {
 
+    @NotBlank
     private String vesselName;
 
+    @NotBlank
     private String shipType;
 
+    @NotBlank
     private String flag;
 
     private String homeport;
 
+    @NotNull
+    @Min(0)
     private Integer grossTonnage;
 
+    @NotNull
+    @Min(0)
     private Integer summerDeadweight;
 
+    @NotNull
+    @Min(0)
     private Double lengthOverall;
 
+    @NotNull
+    @Min(0)
     private Double beam;
 
+    @Min(0)
     private Double draught;
+
+    @NotNull
+    private Integer yearOfBuild;
+
+    private String builder;
+
+    private String placeOfBuild;
 
     private String yard;
 
+    @Min(0)
     private Integer teu;
 
+    @Min(0)
     private Integer crudeOil;
 
+    @Min(0)
     private Double gasCapacity;
-
     private String grain;
 
     private String bale;
@@ -47,15 +73,19 @@ public class UpdateShipRequest {
 
     private String ownerAddress;
 
+    @Email
     private String ownerEmail;
 
+    @URL
     private String ownerWebsite;
 
     private String manager;
 
     private String managerAddress;
 
+    @Email
     private String managerEmail;
 
+    @URL
     private String managerWebsite;
 }
