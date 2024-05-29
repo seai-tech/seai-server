2a99729d7c63065c03a96568d1ad2741b69ea772
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-05-29 09:50:34 +0300

    Moved validators to request models

--- src/main/java/com/seai/ship/model/Ship.java
+++ src/main/java/com/seai/ship/model/Ship.java
@@ -1,56 +1,36 @@
 package com.seai.ship.model;
 
-import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
-import org.hibernate.validator.constraints.URL;
-
 
 @Data
 @AllArgsConstructor
 @Getter
 public class Ship {
 
-    @NotNull
     private String id;
 
-    @NotNull
-    @Digits(integer = 7, fraction = 0)
     private Long imoNumber;
 
-    @NotBlank
     private String vesselName;
 
-    @NotBlank
     private String shipType;
 
-    @NotBlank
     private String flag;
 
     private String homeport;
 
-    @NotNull
-    @Min(0)
     private Integer grossTonnage;
 
-    @NotNull
-    @Min(0)
     private Integer summerDeadweight;
 
-    @NotNull
-    @Min(0)
     private Double lengthOverall;
 
-    @NotNull
-    @Min(0)
     private Double beam;
 
-    @Min(0)
     private Double draught;
 
-    @NotNull
-    @Min(1900)
     private Integer yearOfBuild;
 
     private String builder;
@@ -59,13 +39,10 @@ public class Ship {
 
     private String yard;
 
-    @Min(0)
     private Integer teu;
 
-    @Min(0)
     private Integer crudeOil;
 
-    @Min(0)
     private Double gasCapacity;
 
     private String grain;
@@ -78,19 +55,16 @@ public class Ship {
 
     private String ownerAddress;
 
-    @Email
+
     private String ownerEmail;
 
-    @URL
     private String ownerWebsite;
 
     private String manager;
 
     private String managerAddress;
 
-    @Email
     private String managerEmail;
 
-    @URL
     private String managerWebsite;
 }
