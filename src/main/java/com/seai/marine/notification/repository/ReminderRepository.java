fb461f5773dd313ade85de314194709bfa72da26
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-05-29 09:50:08 +0300

    modified reminder repo to work with the new adj

--- src/main/java/com/seai/marine/notification/repository/ReminderRepository.java
+++ src/main/java/com/seai/marine/notification/repository/ReminderRepository.java
@@ -14,14 +14,14 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class ReminderRepository {
 
-    private static final String CREATE_REMINDER_QUERY = "INSERT INTO reminders (id, user_id, email) VALUES (?, ?, ?)";
+    private static final String CREATE_REMINDER_QUERY = "INSERT INTO reminders (user_id, email) VALUES (?, ?)";
     private static final String DELETE_REMINDER_QUERY = "DELETE FROM reminders WHERE user_id = ?";
-    private static final String FIND_REMINDER_BY_USER_ID_QUERY = "SELECT id, user_id, email FROM reminders WHERE user_id = ?";
+    private static final String FIND_REMINDER_BY_USER_ID_QUERY = "SELECT user_id, email FROM reminders WHERE user_id = ?";
 
     private final JdbcTemplate jdbcTemplate;
 
     public void saveReminder(UUID user_id, String user_email) {
-        jdbcTemplate.update(CREATE_REMINDER_QUERY, UUID.randomUUID(), user_id, user_email);
+        jdbcTemplate.update(CREATE_REMINDER_QUERY, user_id, user_email);
     }
 
     public void deleteReminder(UUID userId) {
@@ -35,7 +35,6 @@ public class ReminderRepository {
 
     private static RowMapper<Reminder> getReminderRowMapper() {
         return (rs, rowNum) -> new Reminder(
-                UUID.fromString(rs.getString("id")),
                 UUID.fromString(rs.getString("user_id")),
                 rs.getString("email")
         );
