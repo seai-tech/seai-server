d0d2faa48d3f34038d255c5aea9747fe72970a86
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-06-10 15:59:06 +0300

    Added service layer

---
+++ src/main/java/com/seai/training_center/course/service/CourseService.java
@@ -0,0 +1,59 @@
+package com.seai.training_center.course.service;
+
+import com.seai.exception.MaxSeatsReachedException;
+import com.seai.exception.ResourceNotFoundException;
+import com.seai.training_center.attendees.repository.AttendeeRepository;
+import com.seai.training_center.course.contract.request.CreateCourseRequest;
+import com.seai.training_center.course.contract.response.GetCourseResponse;
+import com.seai.training_center.course.mapper.CourseMapper;
+import com.seai.training_center.course.model.Course;
+import com.seai.training_center.course.repository.CourseRepository;
+import com.seai.training_center.training_center.repository.TrainingCenterRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class CourseService {
+    private final CourseMapper courseMapper;
+    private final CourseRepository courseRepository;
+    private final TrainingCenterRepository trainingCenterRepository;
+    private final AttendeeRepository attendeeRepository;
+
+    public void createCourse(@RequestBody CreateCourseRequest createCourseRequest, @PathVariable UUID trainingCenterId) {
+        Course course = courseMapper.map(createCourseRequest);
+        courseRepository.save(course, trainingCenterId);
+    }
+
+    public List<GetCourseResponse> getAllCourses() {
+        List<Course> allCourses = courseRepository.findAll();
+        return allCourses.stream().map(courseMapper::map).toList();
+    }
+
+    public GetCourseResponse getCourseById(UUID courseId) {
+        Course course = courseRepository.getCourseById(courseId);
+        return courseMapper.map(course);
+    }
+
+    public void validateCourse(UUID trainingCenterId, UUID courseId) {
+        trainingCenterRepository.findById(trainingCenterId);
+        GetCourseResponse course = getCourseById(courseId);
+        if (!course.getTrainingCenterId().equals(trainingCenterId)) {
+            throw new ResourceNotFoundException("Training Center ID does not match the Course's Training Center ID");
+        }
+    }
+
+    public void validateMaxSeats(UUID courseId) {
+        GetCourseResponse course = getCourseById(courseId);
+        int currentAttendeeCount = attendeeRepository.countByCourseId(courseId);
+        if (currentAttendeeCount >= course.getMaxSeats()) {
+            throw new MaxSeatsReachedException("Cannot add attendee. Maximum number of seats reached for course: " + courseId);
+        }
+    }
+
+}
