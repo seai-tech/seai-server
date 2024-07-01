78f217d3962e22f45cde31b05a0ff1831cde2f8b
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-07-01 10:01:31 +0300

    createCourse method return CreateCourseResponse object(id and trainingId are set here for more clear repository), Getting onto returning availableSeats on course, we set them on runtime for each course, setting it as a attribute on the response object(getAllCourses, getAllCoursesForTrainingCenter, getCourseById), new method for getting the available seats. New method for updating course which handle case when UpdateCourseRequest has less max_seats than current existing attendees. Delete method that delete course and the attendees.

--- src/main/java/com/seai/training_center/course/service/CourseService.java
+++ src/main/java/com/seai/training_center/course/service/CourseService.java
@@ -1,14 +1,15 @@
 package com.seai.training_center.course.service;
 
 import com.seai.exception.MaxSeatsReachedException;
-import com.seai.exception.ResourceNotFoundException;
 import com.seai.training_center.attendees.repository.AttendeeRepository;
+import com.seai.training_center.attendees.service.AttendeeService;
 import com.seai.training_center.course.contract.request.CreateCourseRequest;
+import com.seai.training_center.course.contract.request.UpdateCourseRequest;
+import com.seai.training_center.course.contract.response.CreateCourseResponse;
 import com.seai.training_center.course.contract.response.GetCourseResponse;
 import com.seai.training_center.course.mapper.CourseMapper;
 import com.seai.training_center.course.model.Course;
 import com.seai.training_center.course.repository.CourseRepository;
-import com.seai.training_center.training_center.repository.TrainingCenterRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,40 +17,77 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CourseService {
     private final CourseMapper courseMapper;
     private final CourseRepository courseRepository;
-        private final AttendeeRepository attendeeRepository;
+    private final AttendeeRepository attendeeRepository;
+    private final AttendeeService attendeeService;
 
-    public void createCourse(@RequestBody CreateCourseRequest createCourseRequest, @PathVariable UUID trainingCenterId) {
+    public CreateCourseResponse createCourse(@RequestBody CreateCourseRequest createCourseRequest, @PathVariable UUID trainingCenterId) {
         Course course = courseMapper.map(createCourseRequest);
-        courseRepository.save(course, trainingCenterId);
+        course.setId(UUID.randomUUID());
+        course.setTrainingCenterId(trainingCenterId);
+        return courseMapper.mapToCreateCourseResponse(courseRepository.save(course));
     }
 
     public List<GetCourseResponse> getAllCourses() {
         List<Course> allCourses = courseRepository.findAll();
-        return allCourses.stream().map(courseMapper::map).toList();
+        return allCourses.stream()
+                .map(course -> {
+                    GetCourseResponse courseResponse = courseMapper.map(course);
+                    courseResponse.setAvailableSeats(getAvailable(course));
+                    return courseResponse;
+                })
+                .collect(Collectors.toList());
     }
 
     public List<GetCourseResponse> getAllCoursesForTrainingCenter(UUID trainingCenterId) {
         List<Course> allCourses = courseRepository.findAllForTrainingCenter(trainingCenterId);
-        return allCourses.stream().map(courseMapper::map).toList();
+        return allCourses.stream()
+                .map(course -> {
+                    GetCourseResponse courseResponse = courseMapper.map(course);
+                    courseResponse.setAvailableSeats(getAvailable(course));
+                    return courseResponse;
+                })
+                .collect(Collectors.toList());
     }
 
     public GetCourseResponse getCourseById(UUID trainingCenterId, UUID courseId) {
         Course course = courseRepository.getCourseById(trainingCenterId, courseId);
-        return courseMapper.map(course);
+        GetCourseResponse courseResponse = courseMapper.map(course);
+        courseResponse.setAvailableSeats(getAvailable(course));
+        return courseResponse;
     }
 
+    public int getAvailable(Course course) {
+        int currentAttendeeCount = attendeeRepository.countByCourseId(course.getId());
+        return course.getMaxSeats() - currentAttendeeCount;
+    }
 
     public void validateMaxSeats(GetCourseResponse course) {
         int currentAttendeeCount = attendeeRepository.countByCourseId(course.getId());
         if (currentAttendeeCount >= course.getMaxSeats()) {
             throw new MaxSeatsReachedException("Cannot add attendee. Maximum number of seats reached for course: " + course.getId());
         }
+    }
+
+    public void updateCourse(UUID trainingCenterId, UUID courseId, UpdateCourseRequest updateCourseRequest) {
+        courseRepository.getCourseById(trainingCenterId, courseId);
+        Course updatedCourse = courseMapper.map(updateCourseRequest);
+        int currentAttendeeCount = attendeeRepository.countByCourseId(courseId);
+        if (updatedCourse.getMaxSeats() < currentAttendeeCount) {
+            throw new MaxSeatsReachedException("Cannot update course. Maximum number of seats is less than the current number of attendees");
+        }
+        courseRepository.update(trainingCenterId, courseId, updatedCourse);
+    }
+
+    public void deleteCourse(UUID trainingCenterId, UUID courseId) {
+        attendeeService.deleteAllAttendees(courseId);
+        courseRepository.delete(trainingCenterId, courseId);
     }
 
 }
