f67cbba62a4c9c27bb4a19ce49cf608d7f8dd235
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-07-01 10:07:16 +0300

    New request model for creating course

---
+++ src/main/java/com/seai/training_center/course/contract/request/UpdateCourseRequest.java
@@ -0,0 +1,40 @@
+package com.seai.training_center.course.contract.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.seai.training_center.course.model.CurrencyOptions;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalTime;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class UpdateCourseRequest {
+    private String name;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    @JsonFormat(pattern = "HH:mm:ss")
+    private LocalTime startTime;
+
+    @JsonFormat(pattern = "HH:mm:ss")
+    private LocalTime endTime;
+
+    private BigDecimal price;
+
+    private CurrencyOptions currency;
+
+    private Integer maxSeats;
+
+    private String description;
+
+    private Boolean isPublished;
+}
