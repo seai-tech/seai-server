02b502c1673606fad63d751b5e48fd75c36f6b9c
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-07-04 11:00:51 +0300

    Exception for the token

---
+++ src/main/java/com/seai/exception/TokenException.java
@@ -0,0 +1,8 @@
+package com.seai.exception;
+
+public class TokenException extends GeneralException{
+
+    public TokenException (String message, Object... args) {
+        super(message, args);
+    }
+}
