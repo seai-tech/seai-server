c3e7c617363708b7be569f8a373ccae27d8a1229
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-07-04 10:47:54 +0300

    New custom exception

---
+++ src/main/java/com/seai/exception/ConfirmationException.java
@@ -0,0 +1,8 @@
+package com.seai.exception;
+
+public class ConfirmationException extends GeneralException {
+
+    public ConfirmationException(String message, Object... args) {
+        super(message, args);
+    }
+}
