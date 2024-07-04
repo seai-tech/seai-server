9a1d4fdc68b00f4abc2be2083f9b41e7916e0e78
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-07-04 11:06:24 +0300

    moved the auth validations in the service

--- src/main/java/com/seai/marine/user/controller/UsersAuthController.java
+++ src/main/java/com/seai/marine/user/controller/UsersAuthController.java
@@ -3,35 +3,20 @@ package com.seai.marine.user.controller;
 
 import com.seai.marine.user.contract.request.UserAuthenticationRequest;
 import com.seai.marine.user.contract.response.UserAuthenticationResponse;
-import com.seai.spring.security.model.SecurityUser;
-import com.seai.spring.security.service.JwtService;
+import com.seai.marine.user.service.UserAuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/users")
 public class UsersAuthController {
 
-    private final JwtService jwtService;
-
-    private final AuthenticationManager usersAuthenticationProvider;
+    private final UserAuthService userAuthService;
 
     @PostMapping("/authentication")
-    public UserAuthenticationResponse authenticateAndGetToken(@RequestBody @Valid UserAuthenticationRequest userAuthentaicationRequest) {
-        Authentication authentication = usersAuthenticationProvider.authenticate(new UsernamePasswordAuthenticationToken(userAuthentaicationRequest.getEmail(), userAuthentaicationRequest.getPassword()));
-        if (authentication.isAuthenticated()) {
-            return new UserAuthenticationResponse(((SecurityUser)authentication.getPrincipal()).getId().toString(), jwtService.generateToken(userAuthentaicationRequest.getEmail()));
-        } else {
-            throw new UsernameNotFoundException("invalid user request !");
-        }
+    public UserAuthenticationResponse authenticateAndGetToken(@RequestBody @Valid UserAuthenticationRequest userAuthenticationRequest) {
+        return userAuthService.authenticateAndGetToken(userAuthenticationRequest);
     }
 }
