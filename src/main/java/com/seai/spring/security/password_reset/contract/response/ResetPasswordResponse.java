b86ae56022e6eb46bcfb822f87185e3746a40ce2
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-07-10 10:36:38 +0300

    response model for passwords

---
+++ src/main/java/com/seai/spring/security/password_reset/contract/response/ResetPasswordResponse.java
@@ -0,0 +1,10 @@
+package com.seai.spring.security.password_reset.contract.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResetPasswordResponse {
+    private String message;
+}
