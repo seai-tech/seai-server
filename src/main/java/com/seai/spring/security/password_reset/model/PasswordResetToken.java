717f60a1f29fbe5777569fe64703b2c2cbf0f597
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-07-10 10:35:40 +0300

    new model for password token

---
+++ src/main/java/com/seai/spring/security/password_reset/model/PasswordResetToken.java
@@ -0,0 +1,20 @@
+package com.seai.spring.security.password_reset.model;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class PasswordResetToken {
+    private UUID id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiredAt;
+    private UUID userId;
+}
+
