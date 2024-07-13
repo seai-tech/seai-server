0623ebf8a92e98a39c569e7b6b07c7a9ce3f6e35
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-07-13 18:39:09 +0300

    Added new constructor parameters for the urls and the new TokenGenerator, renamed when needed and added the Optional logic. VerifyToken redirects depends on the case. Not sure about how we'll handle errors, made it to go on /error?message= (maybe the FE need to create error page or something). Messages now are build and send in different classes

---
+++ src/main/java/com/seai/spring/security/email_verification/service/EmailVerificationService.java
@@ -0,0 +1,106 @@
+package com.seai.spring.security.email_verification.service;
+
+import com.seai.exception.*;
+import com.seai.marine.user.model.UserAuthentication;
+import com.seai.marine.user.repository.UserAuthenticationRepository;
+import com.seai.spring.security.email_verification.TokenGenerator;
+import com.seai.spring.security.email_verification.contract.response.TokenConfirmedResponse;
+import com.seai.spring.security.email_verification.message_buildler.ResendConfirmationMessageBuilder;
+import com.seai.spring.security.email_verification.message_buildler.SendConfirmationMessageBuilder;
+import com.seai.spring.security.email_verification.model.VerificationToken;
+import com.seai.spring.security.email_verification.repository.EmailVerificationRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class EmailVerificationService {
+    private final EmailVerificationRepository emailVerificationRepository;
+    private final UserAuthenticationRepository userAuthenticationRepository;
+    private final TokenGenerator tokenGenerator;
+    private final ResendConfirmationMessageBuilder resendConfirmationMessageBuilder;
+    private final SendConfirmationMessageBuilder sendConfirmationMessageBuilder;
+    @Value("${urls.redirect-to-login}")
+    private String redirectToLoginUrl;
+    @Value("${urls.verification-error}")
+    private String verificationErrorUrl;
+    @Value("${urls.verification-token-resend}")
+    private String verificationTokenResendUrl;
+
+
+    public void saveVerificationToken(VerificationToken token) {
+        token.setId(UUID.randomUUID());
+        token.setCreatedAt(LocalDateTime.now());
+        token.setExpiredAt(LocalDateTime.now().plusMinutes(15));
+        emailVerificationRepository.save(token);
+    }
+
+
+    public Optional<VerificationToken> getVerificationToken(String token) {
+        return emailVerificationRepository.findByToken(token);
+    }
+
+
+    public String VerifyToken(String token) {
+        Optional<VerificationToken> confirmationToken = getVerificationToken(token);
+        if (confirmationToken.isEmpty()) {
+            return  verificationErrorUrl + "Token not found";
+        }
+        if (confirmationToken.get().getConfirmedAt() != null) {
+            return redirectToLoginUrl;
+        };
+        if (confirmationToken.get().getExpiredAt().isBefore(LocalDateTime.now())) {
+            return verificationTokenResendUrl;
+        }
+        confirmationToken.get().setConfirmedAt(LocalDateTime.now());
+        emailVerificationRepository.update(confirmationToken.get());
+        sendConfirmationMessageBuilder.sendConfirmationMessage(confirmationToken);
+        return redirectToLoginUrl;
+    }
+
+
+    public Optional<VerificationToken> findByUserId(UUID userId) {
+        return emailVerificationRepository.findByUserId(userId);
+    }
+
+
+    public Optional<VerificationToken> findValidToken(UUID userId) {
+        return emailVerificationRepository.findValidToken(userId, LocalDateTime.now());
+    }
+
+
+    public boolean isEmailConfirmed(UUID userId) {
+        Optional<VerificationToken> confirmationToken = findByUserId(userId);
+        return confirmationToken.isPresent() && confirmationToken.get().getConfirmedAt() != null;
+    }
+
+
+    public TokenConfirmedResponse resendEmailVerificationToken(String email) {
+        UserAuthentication user = userAuthenticationRepository.findByEmail(email);
+        Optional<VerificationToken> existingToken = findValidToken(user.getId());
+        if (isEmailConfirmed(user.getId())) {
+            throw new TokenException("User is already confirmed");
+        }
+        if (existingToken.isPresent()) {
+            throw new TokenException("There is an active token: " + existingToken.get().getToken());
+        }
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setUserId(user.getId());
+        verificationToken.setToken(tokenGenerator.generateToken());
+        saveVerificationToken(verificationToken);
+        resendConfirmationMessageBuilder.resendConfirmationMessage(email, verificationToken.getToken());
+        return new TokenConfirmedResponse("A new verification link has been sent to your email address.");
+    }
+
+    @Scheduled(cron = "${token.scheduler.cron}")
+    public void deleteExpiredTokens() {
+            emailVerificationRepository.deleteAllExpiredTokens(LocalDateTime.now());
+        }
+    }
+
