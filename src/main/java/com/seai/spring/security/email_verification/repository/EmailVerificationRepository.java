890ed405c573100cf4548d9693771fd36b7151bf
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-07-13 18:32:35 +0300

    Renamed the RepositoryClass, optimized delete query, findByUserId and findValidToken are returning Optional, Removed unuseful method for finding expired tokens

--- src/main/java/com/seai/spring/security/email_verification/repository/EmailVerificationRepository.java
+++ src/main/java/com/seai/spring/security/email_verification/repository/EmailVerificationRepository.java
@@ -1,6 +1,6 @@
-package com.seai.spring.security.token.repository;
+package com.seai.spring.security.email_verification.repository;
 
-import com.seai.spring.security.token.model.ConfirmationToken;
+import com.seai.spring.security.email_verification.model.VerificationToken;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -9,13 +9,12 @@ import org.springframework.stereotype.Repository;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
-public class ConfirmationTokenRepository {
+public class EmailVerificationRepository {
 
     private static final String SAVE_TOKEN_QUERY = "INSERT INTO confirmation_tokens (id, token, created_at, expired_at, confirmed_at, user_id) VALUES (?, ?, ?, ?, ?, ?)";
 
@@ -23,9 +22,7 @@ public class ConfirmationTokenRepository {
 
     private static final String FIND_TOKEN_QUERY = "SELECT * FROM confirmation_tokens WHERE token=?";
 
-    private static final String DELETE_TOKEN_QUERY = "DELETE FROM confirmation_tokens WHERE id=?";
-
-    private static final String FIND_EXPIRED_TOKENS_QUERY = "SELECT * FROM confirmation_tokens WHERE expired_at < ? AND confirmed_at IS NULL";
+    private static final String DELETE_EXPIRED_TOKENS_QUERY = "DELETE FROM confirmation_tokens WHERE expired_at < ? AND confirmed_at IS NULL";
 
     private static final String FIND_BY_USER_ID_QUERY = "SELECT * FROM confirmation_tokens WHERE user_id = ? AND confirmed_at IS NOT NULL";
 
@@ -33,7 +30,7 @@ public class ConfirmationTokenRepository {
 
     private final JdbcTemplate jdbcTemplate;
 
-    public ConfirmationToken save(ConfirmationToken token) {
+    public VerificationToken save(VerificationToken token) {
         jdbcTemplate.update(SAVE_TOKEN_QUERY,
                 token.getId(),
                 token.getToken(),
@@ -44,7 +41,7 @@ public class ConfirmationTokenRepository {
         return token;
     }
 
-    public ConfirmationToken update(ConfirmationToken token) {
+    public VerificationToken update(VerificationToken token) {
             jdbcTemplate.update(UPDATE_TOKEN_QUERY,
                     token.getToken(),
                     token.getCreatedAt(),
@@ -55,24 +52,19 @@ public class ConfirmationTokenRepository {
         return token;
     }
 
-    public ConfirmationToken findByToken(String token) {
+    public Optional<VerificationToken> findByToken(String token) {
         try {
-            return jdbcTemplate.queryForObject(FIND_TOKEN_QUERY, tokenRowMapper(), token);
+            return Optional.ofNullable(jdbcTemplate.queryForObject(FIND_TOKEN_QUERY, tokenRowMapper(), token));
         } catch (EmptyResultDataAccessException e) {
-            return null;
+            return Optional.empty();
         }
-
     }
 
-    public void delete(UUID id) {
-        jdbcTemplate.update(DELETE_TOKEN_QUERY, id.toString());
+    public void deleteAllExpiredTokens(LocalDateTime now) {
+        jdbcTemplate.update(DELETE_EXPIRED_TOKENS_QUERY, Timestamp.valueOf(now));
     }
 
-    public List<ConfirmationToken> findAllExpiredTokens(LocalDateTime now) {
-        return jdbcTemplate.query(FIND_EXPIRED_TOKENS_QUERY, tokenRowMapper(), Timestamp.valueOf(now));
-    }
-
-    public Optional<ConfirmationToken> findByUserId(UUID userId) {
+    public Optional<VerificationToken> findByUserId(UUID userId) {
         try {
             return Optional.ofNullable(jdbcTemplate.queryForObject(FIND_BY_USER_ID_QUERY, tokenRowMapper(), userId.toString()));
         } catch (EmptyResultDataAccessException e) {
@@ -80,17 +72,17 @@ public class ConfirmationTokenRepository {
         }
     }
 
-    public ConfirmationToken findValidTokens(UUID userId, LocalDateTime now) {
+    public Optional<VerificationToken> findValidToken(UUID userId, LocalDateTime now) {
         try {
-            return jdbcTemplate.queryForObject(FIND_VALID_TOKENS_QUERY, tokenRowMapper(), Timestamp.valueOf(now), userId.toString());
+            return Optional.ofNullable(jdbcTemplate.queryForObject(FIND_VALID_TOKENS_QUERY, tokenRowMapper(), Timestamp.valueOf(now), userId.toString()));
         } catch (EmptyResultDataAccessException e) {
-            return null;
+            return Optional.empty();
         }
     }
 
 
-    private RowMapper<ConfirmationToken> tokenRowMapper() {
-        return (rs, rowNum) -> new ConfirmationToken(
+    private RowMapper<VerificationToken> tokenRowMapper() {
+        return (rs, rowNum) -> new VerificationToken(
                 UUID.fromString(rs.getString("id")),
                 rs.getString("token"),
                 rs.getTimestamp("created_at").toLocalDateTime(),
