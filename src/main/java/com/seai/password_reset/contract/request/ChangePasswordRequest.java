291a1d7c2aacb9bc237b8d69515760f9c96d550f
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-07-22 10:16:52 +0300

    Added validations in the request model

--- src/main/java/com/seai/password_reset/contract/request/ChangePasswordRequest.java
+++ src/main/java/com/seai/password_reset/contract/request/ChangePasswordRequest.java
@@ -1,7 +1,9 @@
-package com.seai.spring.security.password_reset.contract.request;
+package com.seai.password_reset.contract.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.util.Objects;
 
 @Data
 @AllArgsConstructor
@@ -9,4 +11,8 @@ public class ChangePasswordRequest {
     private String oldPassword;
     private String newPassword;
     private String confirmPassword;
+
+    public boolean arePasswordsMatching() {
+        return Objects.equals(newPassword, confirmPassword);
+    }
 }
