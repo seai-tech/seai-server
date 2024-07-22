ccf7fbc666146df0d81468be18eb23ea6a076564
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-07-22 10:18:11 +0300

    Validations in the model class

--- src/main/java/com/seai/password_reset/contract/request/ForgotPasswordRequest.java
+++ src/main/java/com/seai/password_reset/contract/request/ForgotPasswordRequest.java
@@ -1,11 +1,17 @@
-package com.seai.spring.security.password_reset.contract.request;
+package com.seai.password_reset.contract.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
+import java.util.Objects;
 
 @Data
 @AllArgsConstructor
 public class ForgotPasswordRequest {
     private String newPassword;
     private String confirmPassword;
+
+    public boolean arePasswordsMatching() {
+        return Objects.equals(newPassword, confirmPassword);
+    }
 }
