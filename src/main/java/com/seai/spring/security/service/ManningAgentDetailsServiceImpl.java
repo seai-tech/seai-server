fc9ae32fe8c6ff76d13c76e83781e61e1ab1d7f4
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-08-05 11:27:33 +0300

    New required ManningAgentDetailsServiceImpl

---
+++ src/main/java/com/seai/spring/security/service/ManningAgentDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.seai.spring.security.service;
+
+import com.seai.exception.ResourceNotFoundException;
+import com.seai.manning_agent.manning_agent.model.ManningAgentAuthentication;
+import com.seai.manning_agent.manning_agent.repository.ManningAgentAuthRepository;
+import com.seai.spring.security.model.SecurityUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.Optional;
+
+
+@Service
+@RequiredArgsConstructor
+public class ManningAgentDetailsServiceImpl implements UserDetailsService {
+
+    private final ManningAgentAuthRepository manningAgentAuthRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<ManningAgentAuthentication> manningAgent = Optional.ofNullable(manningAgentAuthRepository.findByEmail(email).orElseThrow(() ->
+                    new ResourceNotFoundException("User with email not found : " + email)));
+        return new SecurityUser(manningAgent.get().getId(), manningAgent.get().getEmail(), manningAgent.get().getPassword()
+                , Collections.emptyList());
+    }
+}
