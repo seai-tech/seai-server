63f34a3fd1d07d3768ef0cbc619c0d51a49ffdb5
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-08-05 11:18:01 +0300

    model for manning agent

---
+++ src/main/java/com/seai/manning_agent/manning_agent/model/ManningAgent.java
@@ -0,0 +1,23 @@
+package com.seai.manning_agent.manning_agent.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+public class ManningAgent {
+
+    private UUID id;
+
+    private String nameOfOrganization;
+
+    private String address;
+
+    private String website;
+
+    private String telephone1;
+
+    private String telephone2;
+}
