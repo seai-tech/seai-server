a78a10dd06c3b5ef6f1bfffb9da61b92b84ab7a6
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-08-07 10:24:16 +0300

    Created Request method so we have json object in field

---
+++ src/main/java/com/seai/password_reset/contract/request/ResetPasswordRequest.java
@@ -0,0 +1,14 @@
+package com.seai.password_reset.contract.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResetPasswordRequest {
+    @NotBlank
+    private String email;
+}
