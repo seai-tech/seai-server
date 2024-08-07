38c4a0b5ea0f8c33b4996b31a5361d3ddfdfaff9
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-08-07 10:12:51 +0300

    Renamed and adjusted the controller method to fit Async process

--- src/main/java/com/seai/marine/email_verification/controller/EmailVerificationController.java
+++ src/main/java/com/seai/marine/email_verification/controller/EmailVerificationController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 @RequiredArgsConstructor
@@ -22,7 +23,7 @@ public class EmailVerificationController {
     }
 
     @PostMapping("/resend-email-verification-token")
-    public TokenConfirmedResponse resendEmailVerificationToken(@RequestParam String email) {
+    public CompletableFuture<TokenConfirmedResponse> resendEmailVerificationToken(@RequestParam String email) {
         return emailVerificationService.resendEmailVerificationToken(email);
     }
 }
