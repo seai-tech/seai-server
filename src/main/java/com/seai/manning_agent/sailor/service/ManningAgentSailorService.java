68374b60c3e11b156677f1106256160de8f8733b
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-08-07 10:16:45 +0300

    create sailor auth with random generated password , @Transactional annot for the delete method

--- src/main/java/com/seai/manning_agent/sailor/service/ManningAgentSailorService.java
+++ src/main/java/com/seai/manning_agent/sailor/service/ManningAgentSailorService.java
@@ -1,13 +1,15 @@
 package com.seai.manning_agent.sailor.service;
 
 import com.seai.exception.ResourceNotFoundException;
-import com.seai.manning_agent.manning_agent.mapper.ManningAgentMapper;
 import com.seai.manning_agent.sailor.contract.request.CreateSailorRequest;
 import com.seai.manning_agent.sailor.mapper.SailorMapper;
 import com.seai.manning_agent.sailor.repository.ManningAgentSailorRepository;
 import com.seai.marine.user.model.User;
+import com.seai.marine.user.model.UserAuthentication;
+import com.seai.marine.user.repository.UserAuthenticationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,6 +22,8 @@ public class ManningAgentSailorService {
     private final SailorMapper sailorMapper;
 
     private final ManningAgentSailorRepository manningAgentSailorRepository;
+
+    private final UserAuthenticationRepository userAuthenticationRepository;
 
 
     public List<User> getAllSailors(UUID manningAgentId) {
@@ -35,11 +39,20 @@ public class ManningAgentSailorService {
         User sailor = sailorMapper.map(sailorRequest);
         sailor.setId(UUID.randomUUID());
         sailor.setManningAgents(manningAgentId.toString());
+        UserAuthentication userAuthentication = sailorMapper.mapToUserAuth(sailorRequest);
+        userAuthentication.setPassword(String.valueOf(UUID.randomUUID()));
+        userAuthenticationRepository.save(userAuthentication, sailor.getId());
         return manningAgentSailorRepository.createSailor(sailor);
     }
 
+    @Transactional
     public void delete(UUID manningAgentId, UUID sailorId) {
         manningAgentSailorRepository.delete(manningAgentId, sailorId);
+        userAuthenticationRepository.delete(sailorId);
     }
+
+
+
+
 
 }
