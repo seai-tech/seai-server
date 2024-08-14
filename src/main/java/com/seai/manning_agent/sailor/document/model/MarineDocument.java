ad855a9e55ca9e25f87b2ad8302db8d1e6582ccf
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-08-14 11:10:36 +0300

    Model for the sailor document

---
+++ src/main/java/com/seai/manning_agent/sailor/document/model/MarineDocument.java
@@ -0,0 +1,44 @@
+package com.seai.manning_agent.sailor.document.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@Getter
+public class MarineDocument {
+    private UUID id;
+    private UUID userId;
+    private String name;
+    private String number;
+    private Date issueDate;
+    private Date expiryDate;
+    private Instant createdDate;
+    private String path;
+    private boolean isVerified;
+
+    public MarineDocument(UUID id, UUID userId, String name, String number, Date issueDate, Date expiryDate, boolean isVerified, Instant createdDate, String path) {
+        this.id = id;
+        this.userId = userId;
+        this.name = name;
+        this.number = number;
+        this.issueDate = issueDate;
+        this.expiryDate = expiryDate;
+        this.isVerified = isVerified;
+        this.createdDate = createdDate;
+        this.path = path;
+    }
+
+    public static MarineDocument createNonVerifiedDocument(String name, String number, Date issueDate, Date expiryDate) {
+        return new MarineDocument(UUID.randomUUID(), null, name, number, issueDate, expiryDate, false, Instant.now(), null);
+    }
+
+    public static MarineDocument createVerifiedDocument(String name, String number, Date issueDate, Date expiryDate) {
+        return new MarineDocument(UUID.randomUUID(), null, name, number, issueDate, expiryDate, true, Instant.now(), null);
+    }
+}
