be0db495000787792bdb1a6157ac844166657406
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-08-29 09:19:47 +0300

    document pattern

--- src/main/java/com/seai/manning_agent/sailor/document/parser/CertificateOfCompetencyDocumentParser.java
+++ src/main/java/com/seai/manning_agent/sailor/document/parser/CertificateOfCompetencyDocumentParser.java
@@ -3,6 +3,7 @@ package com.seai.manning_agent.sailor.document.parser;
 import com.seai.manning_agent.sailor.document.model.MarineDocument;
 import com.seai.manning_agent.sailor.document.parser.util.DocumentSeekUtil;
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
@@ -13,9 +14,12 @@ import java.util.regex.Pattern;
 public class CertificateOfCompetencyDocumentParser implements DocumentParser {
 
     private static final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat("dd.MM.yyyy");
-
+    private static final String documentType = "CERTIFICATE OF COMPETENCY";
     private static final Pattern DATE_PATTERN = Pattern.compile(
             "^\\d{2}\\.\\d{2}\\.\\d{4}$");
+
+    @Value("${document.patterns.certificateNumber}")
+    private String certificateNumberPattern;
 
     @Override
     public boolean canParseDocument(List<String> lines) {
@@ -25,8 +29,7 @@ public class CertificateOfCompetencyDocumentParser implements DocumentParser {
     @SneakyThrows
     @Override
     public MarineDocument parseDocument(List<String> lines) {
-        String documentType = "CERTIFICATE OF COMPETENCY";
-        String number = DocumentSeekUtil.findMatchFor(w -> w.matches("^\\d{7}$"), lines, 1);
+        String number = DocumentSeekUtil.findMatchFor(w -> w.matches(certificateNumberPattern), lines, 1);
         String issueDate = DocumentSeekUtil.findMatchFor(w -> DATE_PATTERN.matcher(w).matches(), lines, 2);
         String validUntil = DocumentSeekUtil.findMatchFor(w -> DATE_PATTERN.matcher(w).matches(), lines, 3);
         return MarineDocument.createNonVerifiedDocument(documentType, number, DATE_FORMATTER.parse(issueDate), DATE_FORMATTER.parse(validUntil));
