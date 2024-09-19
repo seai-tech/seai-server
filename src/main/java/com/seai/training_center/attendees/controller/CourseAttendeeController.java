683a2aa7231f15da7c30ef02c838393d77b72a5e
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-09-19 10:05:18 +0300

    added authorization

--- src/main/java/com/seai/training_center/attendees/controller/CourseAttendeeController.java
+++ src/main/java/com/seai/training_center/attendees/controller/CourseAttendeeController.java
@@ -5,6 +5,7 @@ import com.seai.training_center.attendees.contract.request.UpdateAttendeeRequest
 import com.seai.training_center.attendees.contract.response.GetAttendeeResponse;
 import com.seai.training_center.attendees.service.CourseAttendeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,24 +15,31 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @RequestMapping("/training-centers/{trainingCenterId}/courses/{courseId}/attendees")
 public class CourseAttendeeController {
+
     private final CourseAttendeeService courseAttendeeService;
 
+    private static final String AUTHORIZATION = "#trainingCenterId.equals(authentication.principal.id)";
+
     @PostMapping
+    @PreAuthorize(AUTHORIZATION)
     public void createAttendee(@RequestBody CreateAttendeeRequest createAttendeeRequest, @PathVariable UUID trainingCenterId, @PathVariable UUID courseId) {
         courseAttendeeService.createAttendee(createAttendeeRequest, trainingCenterId, courseId);
     }
 
     @GetMapping
+    @PreAuthorize(AUTHORIZATION)
     public List<GetAttendeeResponse> getAttendees(@PathVariable UUID trainingCenterId, @PathVariable UUID courseId) {
         return courseAttendeeService.getAttendees(trainingCenterId, courseId);
     }
 
     @PutMapping("/{attendeeId}")
+    @PreAuthorize(AUTHORIZATION)
     public void updateAttendee(@RequestBody UpdateAttendeeRequest updateRequest, @PathVariable UUID trainingCenterId, @PathVariable UUID courseId, @PathVariable UUID attendeeId) {
         courseAttendeeService.updateAttendee(updateRequest, trainingCenterId, courseId, attendeeId);
     }
 
     @DeleteMapping("/{attendeeId}")
+    @PreAuthorize(AUTHORIZATION)
     public void deleteAttendee(@PathVariable UUID trainingCenterId, @PathVariable UUID courseId, @PathVariable UUID attendeeId) {
         courseAttendeeService.deleteAttendee(attendeeId);
     }
