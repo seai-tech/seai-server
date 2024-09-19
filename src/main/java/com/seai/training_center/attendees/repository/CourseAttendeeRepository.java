29cdaedfc986c8f750d07b5bc6d7fc8f287bcbf4
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-09-19 09:47:21 +0300

    Modified repository to run with the new attribute

--- src/main/java/com/seai/training_center/attendees/repository/CourseAttendeeRepository.java
+++ src/main/java/com/seai/training_center/attendees/repository/CourseAttendeeRepository.java
@@ -5,7 +5,6 @@ import com.seai.exception.ResourceNotFoundException;
 import com.seai.training_center.attendees.model.Attendee;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DuplicateKeyException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -16,21 +15,21 @@ import java.util.UUID;
 
 @Repository
 @RequiredArgsConstructor
-public class AttendeeRepository {
+public class CourseAttendeeRepository {
 
-    private static final String SAVE_ATTENDEE_QUERY = "INSERT INTO attendees (id, name, email, telephone, remark, is_waiting, course_id, user_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    private static final String SAVE_ATTENDEE_QUERY = "INSERT INTO attendees (id, name, email, telephone, remark, is_waiting, course_id, user_id, online_course_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-    private static final String UPDATE_ATTENDEE_QUERY = "UPDATE attendees SET name=?, email=?, telephone=?, remark=?, is_waiting=?, course_id=?, user_id=? WHERE id=?";
+    private static final String UPDATE_ATTENDEE_QUERY = "UPDATE attendees SET name=?, email=?, telephone=?, remark=?, is_waiting=?, course_id=?, user_id=?, online_course_id WHERE id=?";
 
-    private static final String FIND_ATTENDEES_QUERY = "SELECT id, name, email, telephone, remark, is_waiting, course_id, user_id FROM attendees WHERE course_id=?";
+    private static final String FIND_ATTENDEES_QUERY = "SELECT id, name, email, telephone, remark, is_waiting, course_id, user_id, online_course_id FROM attendees WHERE course_id=?";
 
-    private static final String FIND_ATTENDEE_BY_ID_QUERY = "SELECT id, name, email, telephone, remark, is_waiting, course_id, user_id FROM attendees WHERE id=?";
+    private static final String FIND_ATTENDEE_BY_ID_QUERY = "SELECT id, name, email, telephone, remark, is_waiting, course_id, user_id, online_course_id FROM attendees WHERE id=?";
 
     private static final String DELETE_ATTENDEE_QUERY = "DELETE FROM attendees WHERE id=?";
 
     private static final String DELETE_ATTENDEE_BY_USER_ID_QUERY = "DELETE FROM attendees WHERE course_id=? AND user_id=?";
 
-    private static final String DELETE_ALL_ATTENDEES_QUERY = "DELETE FROM attendees WHERE course_id=?";
+    private static final String DELETE_ALL_ATTENDEES_FOR_COURSE_QUERY = "DELETE FROM attendees WHERE course_id=?";
 
     private static final String COUNT_ATTENDEES_BY_COURSE_ID_QUERY = "SELECT COUNT(*) FROM attendees WHERE course_id=?";
 
@@ -46,7 +45,8 @@ public class AttendeeRepository {
                     attendee.getRemark(),
                     attendee.getIsWaiting(),
                     attendee.getCourseId(),
-                    attendee.getUserId());
+                    attendee.getUserId(),
+                    attendee.getOnlineCourseId());
         } catch (DuplicateKeyException e) {
             throw new DuplicatedResourceException("Attendee already exists");
         }
@@ -61,6 +61,7 @@ public class AttendeeRepository {
                 attendee.getIsWaiting(),
                 attendee.getCourseId(),
                 attendee.getUserId(),
+                attendee.getOnlineCourseId(),
                 attendee.getId().toString());
     }
 
@@ -82,7 +83,7 @@ public class AttendeeRepository {
     }
 
     public void deleteAll(UUID id) {
-        jdbcTemplate.update(DELETE_ALL_ATTENDEES_QUERY, id.toString());
+        jdbcTemplate.update(DELETE_ALL_ATTENDEES_FOR_COURSE_QUERY, id.toString());
     }
 
     public int countByCourseId(UUID courseId) {
@@ -98,6 +99,7 @@ public class AttendeeRepository {
                 rs.getString("remark"),
                 rs.getBoolean("is_waiting"),
                 UUID.fromString(rs.getString("course_id")),
-                rs.getObject("user_id") != null ? UUID.fromString(rs.getString("user_id")) : null);
+                rs.getObject("user_id") != null ? UUID.fromString(rs.getString("user_id")) : null,
+                rs.getObject("online_course_id") != null ? UUID.fromString(rs.getString("user_id")) : null);
     }
 }
