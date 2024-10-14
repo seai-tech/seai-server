01acf281d3b28d4a6c3757363f04f78291afec87
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-10-14 19:31:32 +0300

    request models for user and manning agent

---
+++ src/main/java/com/seai/manning_agent/sailor/next_of_kin/contract/request/NextOfKinUpdateRequest.java
@@ -0,0 +1,21 @@
+package com.seai.manning_agent.sailor.next_of_kin.contract.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NextOfKinUpdateRequest {
+
+    private String name;
+
+    private String surname;
+
+    private String phone;
+
+    private String address;
+
+    private String email;
+}
