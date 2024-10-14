a279ee3c2c5a80e72e7f2fc6a1d03d909f936005
GeorgiStefanov99 <133319696+GeorgiStefanov99@users.noreply.github.com>
2024-10-14 19:30:50 +0300

    create request for next of kin

---
+++ src/main/java/com/seai/manning_agent/sailor/next_of_kin/contract/request/NextOfKinCreateRequest.java
@@ -0,0 +1,21 @@
+package com.seai.manning_agent.sailor.next_of_kin.contract.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class NextOfKinCreateRequest {
+
+    private String name;
+
+    private String surname;
+
+    private String phone;
+
+    private String address;
+
+    private String email;
+}
